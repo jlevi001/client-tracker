4f3ab6f86cecf1828d75728db0b95766ab461eab
jlevi001 <james@lingoit.net>
2025-09-09 07:03:03 -0500

    component migration

--- resources/views/components/action-section.blade.php
+++ resources/views/components/action-section.blade.php
@@ -5,8 +5,10 @@
     </x-section-title>
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            {{ $content }}
+        <div class="card bg-base-200 shadow-xl">
+            <div class="card-body">
+                {{ $content }}
+            </div>
         </div>
     </div>
 </div>
--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -8,17 +8,19 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit="{{ $submit }}">
-            <div class="px-4 py-5 bg-white dark:bg-gray-800 sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
-                <div class="grid grid-cols-6 gap-6">
-                    {{ $form }}
+            <div class="card bg-base-200 shadow-xl">
+                <div class="card-body">
+                    <div class="grid grid-cols-6 gap-6">
+                        {{ $form }}
+                    </div>
                 </div>
+                
+                @if (isset($actions))
+                    <div class="card-actions justify-end bg-base-300 px-6 py-3">
+                        {{ $actions }}
+                    </div>
+                @endif
             </div>
-
-            @if (isset($actions))
-                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 dark:bg-gray-800 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
-                    {{ $actions }}
-                </div>
-            @endif
         </form>
     </div>
 </div>
