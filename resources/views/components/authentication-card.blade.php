727a3f978854c6799a91ecde1d7a8864855dae32
jlevi001 <james@lingoit.net>
2025-09-09 07:43:03 -0500

    Auth Views Migration

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-base-200">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 card bg-base-100 shadow-xl overflow-hidden">
+        <div class="card-body">
+            {{ $slot }}
+        </div>
     </div>
 </div>
